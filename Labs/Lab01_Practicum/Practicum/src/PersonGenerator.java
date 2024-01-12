ec682b1c3f37bd11b0b43dbf723ef3bd87c917bf
DMKALALA <96592192+DMKALALA@users.noreply.github.com>
2024-01-12 02:51:18 -0500

    Updated code for Person generator and added data.txt

--- Labs/Lab01_Practicum/Practicum/src/PersonGenerator.java
+++ Labs/Lab01_Practicum/Practicum/src/PersonGenerator.java
@@ -6,6 +6,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static java.nio.file.StandardOpenOption.CREATE;
+
 public class PersonGenerator {
     public static void main(String[] args) {
         ArrayList <String>data = new ArrayList<>();
@@ -33,9 +35,37 @@ public class PersonGenerator {
            done = SafeInput.getYNConfirm(in,"Are you done adding data?");
         }while (!done);
 
-        for(String people:data){
-            System.out.println(people);
+//        for(String people:data){
+//            System.out.println(people);
+//
+//        }
 
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\data.txt");
+
+        try
+        {
+            // Typical java pattern of inherited classes
+            // we wrap a BufferedWriter around a lower level BufferedOutputStream
+            OutputStream out = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+
+            // Finally can write the file LOL!
+
+            for(String text : data)
+            {
+                writer.write(text, 0, text.length());  // stupid syntax for write rec
+                // 0 is where to start (1st char) the write
+                // rec. length() is how many chars to write (all)
+                writer.newLine();  // adds the new line
+
+            }
+            writer.close(); // must close the file to seal it and flush buffer
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
         }
     }
 
