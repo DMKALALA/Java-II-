13ec5b868246ed9eef816653dada4c0afbb62c40
DMKALALA <96592192+DMKALALA@users.noreply.github.com>
2024-01-18 21:14:28 -0500

    Person class and Safeinput class added

---
+++ Labs/Lab_01_Get_Classy/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private  String Person = "";
+    private String ID = "";
+    private String firstName = "";
+    private String lastName = "";
+    private String title = "";
+    private int YOB = 0;
+
+    public Person(String person ,String firstName,String lastName,String title, int yearOfBirth ){
+        this.Person = person;
+        this.firstName = firstName;
+
+
+    }
+
+
+}
