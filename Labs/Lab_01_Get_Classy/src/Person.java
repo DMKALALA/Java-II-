5935c9a54d448ba12b1f27c64c592d988ad74698
DMKALALA <96592192+DMKALALA@users.noreply.github.com>
2024-01-18 21:41:28 -0500

    tested my code

--- Labs/Lab_01_Get_Classy/src/Person.java
+++ Labs/Lab_01_Get_Classy/src/Person.java
@@ -1,17 +1,71 @@
+package src;
+
 public class Person {
-    private  String Person = "";
-    private String ID = "";
+
+    private String IDNum = "";
     private String firstName = "";
     private String lastName = "";
     private String title = "";
     private int YOB = 0;
 
-    public Person(String person ,String firstName,String lastName,String title, int yearOfBirth ){
-        this.Person = person;
+    public Person( String IDNum, String firstName,String lastName,String title, int YOB ){
+        this.IDNum = IDNum;
         this.firstName = firstName;
+        this.lastName = lastName;
+        this.title = title;
+        this.YOB = YOB;
 
 
     }
 
+    public String getIDNum() {
+        return IDNum;
+    }
 
+    public void setIDNum(String IDNum) {
+        this.IDNum = IDNum;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYOB() {
+        return YOB;
+    }
+
+    public void setYOB(int YOB) {
+        this.YOB = YOB;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "IDNum='" + IDNum + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", title='" + title + '\'' +
+                ", YOB=" + YOB +
+                '}';
+    }
 }
---
+++ Labs/Lab_01_Get_Classy/src/PersonGenerator.java
@@ -0,0 +1,8 @@
+package src;
+
+public class PersonGenerator {
+    public static void main(String[] args) {
+        Person sally = new Person("","","","",2);
+        System.out.println(sally);
+    }
+}
