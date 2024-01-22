3e8ddea257a222763d1b21ea0f3dbb646b38bbce
DMKALALA <96592192+DMKALALA@users.noreply.github.com>
2024-01-22 15:17:31 -0500

    Add the test files

--- Labs/Lab_01_Get_Classy/test/PersonTest.java
+++ Labs/Lab_01_Get_Classy/test/PersonTest.java
@@ -1,35 +1,4 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 class PersonTest {
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @Test
-    void fullName() {
-    }
-
-    @Test
-    void formalName() {
-    }
-
-    @Test
-    void getAge() {
-    }
-
-    @Test
-    void testGetAge() {
-    }
-
-    @Test
-    void toCSVDataRecord() {
-    }
-
-    @Test
-    void testToString() {
-    }
+  
 }
