3484ea38cd236f3fba763b2c4801d1b0ff2d6d25
DMKALALA <96592192+DMKALALA@users.noreply.github.com>
2024-01-22 14:54:27 -0500

    ProductTest file added

---
+++ Labs/Lab_01_Get_Classy/test/ProductTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @Test
+    void toCSVDataRecord() {
+    }
+
+    @Test
+    void setIDSeed() {
+    }
+
+    @Test
+    void getIDSeed() {
+    }
+
+    @Test
+    void genIDNum() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getIDNum() {
+    }
+
+    @Test
+    void setIDNum() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void getCost() {
+    }
+
+    @Test
+    void setCost() {
+    }
+}
